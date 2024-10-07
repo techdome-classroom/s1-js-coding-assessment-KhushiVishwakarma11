35bf86c92e6b7daa07d116b1f19c3aa12c221e0d
KhushiVishwakarma11 <kushivishwakarma11@gmail.com>
2024-10-07 12:35:06 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,26 @@
 const decodeTheRing = function (s, p) {
+    const m = s.length, n = p.length;
+    const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
 
-    // write your code here
+    dp[0][0] = true;
 
-  };
-  
-  module.exports = decodeTheRing;
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[m][n];
+};
+
+module.exports = decodeTheRing;
